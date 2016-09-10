afb23b02e905df553c9eb9d670e0a813dc047940
Noah Prail <nprail@filiosoft.com>
2016-09-10 12:48:27 -0400

    Completed #1

--- www/js/app.js
+++ www/js/app.js
@@ -4,60 +4,69 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.ulrs', {
-    url: '/urls',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/urls.html'
       }
-    }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
   })
 
-  .state('app.shorten', {
-      url: '/shorten',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/shorten.html'
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+      })
+
+      .state('app.urls', {
+        url: '/urls',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/urls.html'
+          }
         }
-      }
-    })
-  .state('app.single', {
-    url: '/urls/:urlId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/urlDetails.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
+      })
+
+      .state('app.shorten', {
+        url: '/shorten',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/shorten.html'
+          }
+        }
+      })
+      .state('app.settings', {
+        url: '/settings',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/settings.html',
+            controller: 'SettingsCtrl'
+          }
+        }
+      })
+      .state('app.single', {
+        url: '/urls/:urlId',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/urlDetails.html',
+            controller: 'PlaylistCtrl'
+          }
+        }
+      });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/urls');
   });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/urls');
-});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,58 +1,73 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, configService) {
+  .controller('AppCtrl', function ($scope, $ionicModal, $timeout, configService, $window) {
+    $scope.loggedIn = false;
+    var url = configService.get('url');
+    var api = configService.get('apikey');
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    if (url && api) {
+      $scope.loggedIn = true;
 
-  // Form data for the login modal
-  $scope.loginData = {};
+      console.log("Logged in!")
+    }
+    else {
+      $scope.loggedIn = false;
+      console.log("Not logged in. ")
+    }
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
+    $scope.loginData = {};
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
+
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+      $scope.modal.hide();
+    };
+
+    // Open the login modal
+    $scope.login = function () {
+      $scope.modal.show();
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+      console.log('Doing login', $scope.loginData);
+
+      configService.set('url', $scope.loginData.url);
+      configService.set('apikey', $scope.loginData.apikey);
+
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function () {
+        $scope.closeLogin();
+        $window.location.reload(true)
+      }, 1000);
+    };
+
+  })
+
+  .controller('PlaylistsCtrl', function ($scope) {
+    $scope.playlists = [
+      { title: 'Reggae', id: 1 },
+      { title: 'Chill', id: 2 },
+      { title: 'Dubstep', id: 3 },
+      { title: 'Indie', id: 4 },
+      { title: 'Rap', id: 5 },
+      { title: 'Cowbell', id: 6 }
+    ];
+  })
+
+  .controller('SettingsCtrl', function ($scope, $window, configService, $state) {
+    $scope.doLogout = function () {
+      configService.destroy('apikey');
+      configService.destroy('url');
+
+      $window.location.reload(true);
+      $state.go('app.urls', {}, {reload: true});
+    }
   });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
