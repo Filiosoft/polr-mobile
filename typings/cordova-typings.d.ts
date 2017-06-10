3b55ab844a6553a3676e47f7968ea49b714df0c7
Noah Prail <nprail@filiosoft.com>
2017-06-10 15:47:04 -0400

    Added crosswalk

--- typings/cordova-typings.d.ts
+++ typings/cordova-typings.d.ts
@@ -4,3 +4,6 @@
 /// <reference path="..\.vscode\typings\ionic\ionic.d.ts"/>
 /// <reference path="..\.vscode\typings\jquery\jquery.d.ts"/>
 /// <reference path="..\.vscode\typings\cordova\cordova.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Device.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Splashscreen.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\StatusBar.d.ts"/>
